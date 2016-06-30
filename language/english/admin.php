b7086ecf277adfd128551f831f1866ac24472139
mambax7 <mambax7@gmail.com>
2016-06-30 06:04:08 -0400

    quotation marks

--- language/english/admin.php
+++ language/english/admin.php
@@ -17,59 +17,59 @@
  * @author          Mage Gregory (AKA Mage)
  */
 // all
-define('_AM_XMCONTACT_ACTION', "Action");
-define('_AM_XMCONTACT_ADD', "Add");
-define('_AM_XMCONTACT_CATEGORY', "Category");
-define('_AM_XMCONTACT_DEL', "Delete");
-define('_AM_XMCONTACT_EDIT', "Edit");
-define('_AM_XMCONTACT_EDITSTATUS', "Manually edit the status");
-define('_AM_XMCONTACT_REDIRECT_SAVE', "Successfully saved");
-define('_AM_XMCONTACT_REPLY', "Reply");
-define('_AM_XMCONTACT_STATUS', "Status");
-define('_AM_SYSTEM_STATUS_OFF', "Disabled");
-define('_AM_SYSTEM_STATUS_ON', "Active");
-define('_AM_XMCONTACT_VIEW', "View Details");
+define('_AM_XMCONTACT_ACTION', 'Action');
+define('_AM_XMCONTACT_ADD', 'Add');
+define('_AM_XMCONTACT_CATEGORY', 'Category');
+define('_AM_XMCONTACT_DEL', 'Delete');
+define('_AM_XMCONTACT_EDIT', 'Edit');
+define('_AM_XMCONTACT_EDITSTATUS', 'Manually edit the status');
+define('_AM_XMCONTACT_REDIRECT_SAVE', 'Successfully saved');
+define('_AM_XMCONTACT_REPLY', 'Reply');
+define('_AM_XMCONTACT_STATUS', 'Status');
+define('_AM_SYSTEM_STATUS_OFF', 'Disabled');
+define('_AM_SYSTEM_STATUS_ON', 'Active');
+define('_AM_XMCONTACT_VIEW', 'View Details');
 
 // index
-define('_AM_XMCONTACT_INDEX_CAT', "Categories");
-define('_AM_XMCONTACT_INDEX_CAT_ACTIVE', "%s active categories");
-define('_AM_XMCONTACT_INDEX_CAT_NOTACTIVE', "%s disabled categories");
-define('_AM_XMCONTACT_INDEX_CAT_NOTREPLY', "%s unaswered requests");
-define('_AM_XMCONTACT_INDEX_REQUEST', "Requests");
-define('_AM_XMCONTACT_INDEX_REQUEST_REPLY', "%s processed requests");
+define('_AM_XMCONTACT_INDEX_CAT', 'Categories');
+define('_AM_XMCONTACT_INDEX_CAT_ACTIVE', '%s active categories');
+define('_AM_XMCONTACT_INDEX_CAT_NOTACTIVE', '%s disabled categories');
+define('_AM_XMCONTACT_INDEX_CAT_NOTREPLY', '%s unaswered requests');
+define('_AM_XMCONTACT_INDEX_REQUEST', 'Requests');
+define('_AM_XMCONTACT_INDEX_REQUEST_REPLY', '%s processed requests');
 
 // request
-define('_AM_XMCONTACT_REQUEST_DATER', "Date received");
-define('_AM_XMCONTACT_REQUEST_DATES', "Date responded");
-define('_AM_XMCONTACT_REQUEST_EMAIL', "Email");
-define('_AM_XMCONTACT_REQUEST_PHONE', "Telephone");
-define('_AM_XMCONTACT_REQUEST_FROM', "From:");
-define('_AM_XMCONTACT_REQUEST_INFORMATION', "Informations");
-define('_AM_XMCONTACT_REQUEST_IP', "IP");
-define('_AM_XMCONTACT_REQUEST_LIST', "List of requests");
-define('_AM_XMCONTACT_REQUEST_MESSAGE', "Message");
-define('_AM_XMCONTACT_REQUEST_SENDEMAIL', "Email sent");
-define('_AM_XMCONTACT_REQUEST_STATUS_NR', "No answer");
-define('_AM_XMCONTACT_REQUEST_STATUS_R', "Answered");
-define('_AM_XMCONTACT_REQUEST_SUBJECT', "Subject");
-define('_AM_XMCONTACT_REQUEST_SUBMITTER', "Name");
-define('_AM_XMCONTACT_REQUEST_SUREDEL', "Sure to delete this request? %s");
-define('_AM_XMCONTACT_REQUEST_TITLE', "Title");
-define('_AM_XMCONTACT_REQUEST_TO', "A:");
+define('_AM_XMCONTACT_REQUEST_DATER', 'Date received');
+define('_AM_XMCONTACT_REQUEST_DATES', 'Date responded');
+define('_AM_XMCONTACT_REQUEST_EMAIL', 'Email');
+define('_AM_XMCONTACT_REQUEST_PHONE', 'Telephone');
+define('_AM_XMCONTACT_REQUEST_FROM', 'From:');
+define('_AM_XMCONTACT_REQUEST_INFORMATION', 'Informations');
+define('_AM_XMCONTACT_REQUEST_IP', 'IP');
+define('_AM_XMCONTACT_REQUEST_LIST', 'List of requests');
+define('_AM_XMCONTACT_REQUEST_MESSAGE', 'Message');
+define('_AM_XMCONTACT_REQUEST_SENDEMAIL', 'Email sent');
+define('_AM_XMCONTACT_REQUEST_STATUS_NR', 'No answer');
+define('_AM_XMCONTACT_REQUEST_STATUS_R', 'Answered');
+define('_AM_XMCONTACT_REQUEST_SUBJECT', 'Subject');
+define('_AM_XMCONTACT_REQUEST_SUBMITTER', 'Name');
+define('_AM_XMCONTACT_REQUEST_SUREDEL', 'Sure to delete this request? %s');
+define('_AM_XMCONTACT_REQUEST_TITLE', 'Title');
+define('_AM_XMCONTACT_REQUEST_TO', 'A:');
 
 // category
-define('_AM_XMCONTACT_CATEGORY_ADD', "Add Category");
-define('_AM_XMCONTACT_CATEGORY_DESC', "Description");
-define('_AM_XMCONTACT_CATEGORY_FORMPATH', "Files are in: %s");
-define('_AM_XMCONTACT_CATEGORY_LIST', "Category List");
-define('_AM_XMCONTACT_CATEGORY_LOGO', "Logo");
-define('_AM_XMCONTACT_CATEGORY_LOGOFILE', "Logo file");
-define('_AM_XMCONTACT_CATEGORY_RESPONSIBLE', "Owner");
-define('_AM_XMCONTACT_CATEGORY_SUREDEL', "Sure to delete this category? %s");
-define('_AM_XMCONTACT_CATEGORY_TITLE', "Title");
-define('_AM_XMCONTACT_CATEGORY_UPLOAD', "Upload");
-define('_AM_XMCONTACT_CATEGORY_WEIGHT', "Weight");
+define('_AM_XMCONTACT_CATEGORY_ADD', 'Add Category');
+define('_AM_XMCONTACT_CATEGORY_DESC', 'Description');
+define('_AM_XMCONTACT_CATEGORY_FORMPATH', 'Files are in: %s');
+define('_AM_XMCONTACT_CATEGORY_LIST', 'Category List');
+define('_AM_XMCONTACT_CATEGORY_LOGO', 'Logo');
+define('_AM_XMCONTACT_CATEGORY_LOGOFILE', 'Logo file');
+define('_AM_XMCONTACT_CATEGORY_RESPONSIBLE', 'Owner');
+define('_AM_XMCONTACT_CATEGORY_SUREDEL', 'Sure to delete this category? %s');
+define('_AM_XMCONTACT_CATEGORY_TITLE', 'Title');
+define('_AM_XMCONTACT_CATEGORY_UPLOAD', 'Upload');
+define('_AM_XMCONTACT_CATEGORY_WEIGHT', 'Weight');
 
 // error
-define('_AM_XMCONTACT_ERROR_CAT', "There are no categories in the database");
-define('_AM_XMCONTACT_ERROR_WEIGHT', "Weight must be a number");
+define('_AM_XMCONTACT_ERROR_CAT', 'There are no categories in the database');
+define('_AM_XMCONTACT_ERROR_WEIGHT', 'Weight must be a number');
--- language/english/modinfo.php
+++ language/english/modinfo.php
@@ -17,35 +17,35 @@
  * @author          Mage Gregory (AKA Mage)
  */
 // The name of this module
-define('_MI_XMCONTACT_NAME', "Contact Us");
-define('_MI_XMCONTACT_DESC', "Contact module");
+define('_MI_XMCONTACT_NAME', 'Contact Us');
+define('_MI_XMCONTACT_DESC', 'Contact module');
 
 // Admin menu
-define('_MI_XMCONTACT_MENU_HOME', "Home");
-define('_MI_XMCONTACT_MENU_HOME_DESC', "Go back to homepage");
-define('_MI_XMCONTACT_MENU_CATEGORY', "Categories");
-define('_MI_XMCONTACT_MENU_CATEGORY_DESC', "List of categories");
-define('_MI_XMCONTACT_MENU_REQUEST', "Requests");
-define('_MI_XMCONTACT_MENU_REQUEST_DESC', "List of requests");
-define('_MI_XMCONTACT_MENU_ABOUT', "About");
-define('_MI_XMCONTACT_MENU_ABOUT_DESC', "About this module");
-define('_MI_XMCONTACT_MENU_HELP', "Help");
-define('_MI_XMCONTACT_MENU_HELP_DESC', "Module help");
+define('_MI_XMCONTACT_MENU_HOME', 'Home');
+define('_MI_XMCONTACT_MENU_HOME_DESC', 'Go back to homepage');
+define('_MI_XMCONTACT_MENU_CATEGORY', 'Categories');
+define('_MI_XMCONTACT_MENU_CATEGORY_DESC', 'List of categories');
+define('_MI_XMCONTACT_MENU_REQUEST', 'Requests');
+define('_MI_XMCONTACT_MENU_REQUEST_DESC', 'List of requests');
+define('_MI_XMCONTACT_MENU_ABOUT', 'About');
+define('_MI_XMCONTACT_MENU_ABOUT_DESC', 'About this module');
+define('_MI_XMCONTACT_MENU_HELP', 'Help');
+define('_MI_XMCONTACT_MENU_HELP_DESC', 'Module help');
 // Pref.
 define('_MI_XMCONTACT_PREF_HEAD_INFORMATION', "<span style='font-weight: bold;'>Information</span>");
-define('_MI_XMCONTACT_PREF_COLUMNCAT', "Number of column for Category View");
-define('_MI_XMCONTACT_PREF_COLUMNCAT_DESC', "Number of categories that can be viewed: 1, 2, 3 or 4 columns");
-define('_MI_XMCONTACT_PREF_HEADER', "Header index page");
-define('_MI_XMCONTACT_PREF_HEADER_DESC', "Set HTML codes to show in contact page");
-define('_MI_XMCONTACT_PREF_FOOTER', "Footer contact form");
-define('_MI_XMCONTACT_PREF_FOOTER_DESC', "Set HTML codes to show in contact page");
-define('_MI_XMCONTACT_PREF_ADDRESSE', "Addresse index page");
-define('_MI_XMCONTACT_PREF_ADDRESSE_DESC', "Set HTML codes to show in contact page");
-define('_MI_XMCONTACT_PREF_GOOGLEMAPS', "Embed Google maps");
+define('_MI_XMCONTACT_PREF_COLUMNCAT', 'Number of column for Category View');
+define('_MI_XMCONTACT_PREF_COLUMNCAT_DESC', 'Number of categories that can be viewed: 1, 2, 3 or 4 columns');
+define('_MI_XMCONTACT_PREF_HEADER', 'Header index page');
+define('_MI_XMCONTACT_PREF_HEADER_DESC', 'Set HTML codes to show in contact page');
+define('_MI_XMCONTACT_PREF_FOOTER', 'Footer contact form');
+define('_MI_XMCONTACT_PREF_FOOTER_DESC', 'Set HTML codes to show in contact page');
+define('_MI_XMCONTACT_PREF_ADDRESSE', 'Addresse index page');
+define('_MI_XMCONTACT_PREF_ADDRESSE_DESC', 'Set HTML codes to show in contact page');
+define('_MI_XMCONTACT_PREF_GOOGLEMAPS', 'Embed Google maps');
 define('_MI_XMCONTACT_PREF_GOOGLEMAPS_DESC', "Embed Google maps iFrame<br />change iFrame width to '100%'");
-define('_MI_XMCONTACT_PREF_NOTIFICATION', "Enable email notification");
-define('_MI_XMCONTACT_PREF_NOTIFICATION_DESC', "For every contact request, the category owner will receive an email notification");
+define('_MI_XMCONTACT_PREF_NOTIFICATION', 'Enable email notification');
+define('_MI_XMCONTACT_PREF_NOTIFICATION_DESC', 'For every contact request, the category owner will receive an email notification');
 define('_MI_XMCONTACT_PREF_HEAD_ADMIN', "<span style='font-weight: bold;'>Administration</span>");
-define('_MI_XMCONTACT_PREF_EDITOR', "Text Editor");
-define('_MI_XMCONTACT_PREF_ITEMPERPAGE', "Number of items per page in the Admin view");
+define('_MI_XMCONTACT_PREF_EDITOR', 'Text Editor');
+define('_MI_XMCONTACT_PREF_ITEMPERPAGE', 'Number of items per page in the Admin view');
 
