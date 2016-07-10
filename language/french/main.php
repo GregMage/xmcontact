0bd4944937516762d2e22378668c5d42c753ea81
Christelle Gaulupeau <cesagxoops@gmail.com>
2016-07-10 11:36:18 +0200

    Update main.php

--- language/french/main.php
+++ language/french/main.php
@@ -18,8 +18,8 @@
  */
 // index.php
 define('_MD_XMCONTACT_INDEX_CONTACT', 'Contact');
-define('_MD_XMCONTACT_INDEX_EMAIL', 'Adresse email');
-define('_MD_XMCONTACT_INDEX_EMAIL_PH', 'Entrez votre Adresse email');
+define('_MD_XMCONTACT_INDEX_EMAIL', 'Adresse électronique');
+define('_MD_XMCONTACT_INDEX_EMAIL_PH', 'Entrez votre adresse électronique');
 define('_MD_XMCONTACT_INDEX_FORM', 'Formulaire');
 define('_MD_XMCONTACT_INDEX_LISTCONTACT', 'Liste des contacts');
 define('_MD_XMCONTACT_INDEX_MAIL_SUBJECT', 'Nouvelle demande de contact');
@@ -28,16 +28,16 @@ define('_MD_XMCONTACT_INDEX_MESSAGE_PH', 'Entrez votre message');
 define('_MD_XMCONTACT_INDEX_NAME', 'Nom');
 define('_MD_XMCONTACT_INDEX_NAME_PH', 'Entrez votre nom');
 define('_MD_XMCONTACT_INDEX_PHONE', 'Téléphone');
-define('_MD_XMCONTACT_INDEX_PHONE_PH', 'Entrez votre Téléphone');
+define('_MD_XMCONTACT_INDEX_PHONE_PH', 'Entrez votre numéro de téléphone');
 define('_MD_XMCONTACT_INDEX_SUBMIT', 'Envoyer');
 define('_MD_XMCONTACT_INDEX_SUBJECT', 'Sujet');
 define('_MD_XMCONTACT_INDEX_SUBJECT_PH', 'Entrez votre sujet');
 define('_MD_XMCONTACT_REDIRECT_SEND', 'Message envoyé');
 
 //errors
-define('_MD_XMCONTACT_ERROR_CAPTCHA', 'Le captcha n\'est pas correct!');
-define('_MD_XMCONTACT_ERROR_EMAIL', 'L\'email est obligatoire et doit être valide!');
-define('_MD_XMCONTACT_ERROR_MESSAGE', 'Le message est obligatoire!');
-define('_MD_XMCONTACT_ERROR_NAME', 'Le nom est obligatoire!');
-define('_MD_XMCONTACT_ERROR_NOCAPTCHA', 'Le captcha est obligatoire!');
-define('_MD_XMCONTACT_ERROR_SUBJECT', 'Le sujet est obligatoire!');
+define('_MD_XMCONTACT_ERROR_CAPTCHA', 'Le captcha n\'est pas correct !');
+define('_MD_XMCONTACT_ERROR_EMAIL', 'Le courriel est obligatoire et doit être valide !');
+define('_MD_XMCONTACT_ERROR_MESSAGE', 'Le message est obligatoire !');
+define('_MD_XMCONTACT_ERROR_NAME', 'Le nom est obligatoire !');
+define('_MD_XMCONTACT_ERROR_NOCAPTCHA', 'Le captcha est obligatoire !');
+define('_MD_XMCONTACT_ERROR_SUBJECT', 'Le sujet est obligatoire !');
