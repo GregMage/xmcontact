6c12110918c0c6655b3f49eedfa4d812d81afd89
Grégory Mage <gregory.mage@gmail.com>
2020-04-30 19:38:03 +0200

    Version 1.1 RC 1

--- xoops_version.php
+++ xoops_version.php
@@ -18,7 +18,7 @@
  */
 
 $modversion['name']                = _MI_XMCONTACT_NAME;
-$modversion['version']             = '1.0';
+$modversion['version']             = '1.1';
 $modversion['description']         = _MI_XMCONTACT_DESC;
 $modversion['credits']             = 'G. Mage';
 $modversion['author']              = 'G. Mage';
@@ -34,10 +34,10 @@ $modversion['icons32']             = 'Frameworks/moduleclasses/icons/32';
 $modversion['help']                = 'page=help';
 
 //about
-$modversion['release_date']         = '2020/04/24';
+$modversion['release_date']         = '2020/04/30';
 $modversion['module_website_url']   = 'www.monxoops.fr/';
 $modversion['module_website_name']  = 'MonXoops';
-$modversion['module_status']        = 'Final';
+$modversion['module_status']        = 'RC 1';
 $modversion['min_xoops'] 			= '2.5.10';
 $modversion['min_php']   			= '7.0';
 $modversion['min_db']    			= ['mysql' => '5.5'];
