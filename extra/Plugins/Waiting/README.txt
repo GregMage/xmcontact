21e83f9eef7fde8ddd471188a1090c09acd595d4
Grégory Mage <gregory.mage@gmail.com>
2020-11-08 12:29:17 +0100

    Ajout: Plugin pour le module waiting

---
+++ extra/Plugins/Waiting/README.txt
@@ -0,0 +1,13 @@
+Important: The latest version of waiting already includes this plugin!
+
+Copy the plugins from this folder to the folder:
+
+htdocs\modules\waiting\plugins
+
+And add this define:
+
+define('_PI_WAITING_CONTACT', 'Contact pending response');
+
+in:
+
+htdocs\modules\waiting\language\english\plugins.php
---
+++ extra/Plugins/Waiting/xmcontact.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ You may not change or alter any portion of this comment or credits
+ of supporting developers from this source code or any supporting source code
+ which is considered copyrighted (c) material of the original comment or credit authors.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+/**
+ * xmcontact module
+ *
+ * @copyright       XOOPS Project (https://xoops.org)
+ * @license         GNU GPL 2 (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
+ * @author          Mage Gregory (AKA Mage)
+ */
+ 
+/**
+ * @return array
+ */
+function b_waiting_xmcontact()
+{
+    /** @var \XoopsMySQLDatabase $xoopsDB */
+    $xoopsDB = \XoopsDatabaseFactory::getDatabaseConnection();
+    $ret     = [];
+    $block   = [];
+    // extcal events
+    $sql    = 'SELECT COUNT(*) FROM ' . $xoopsDB->prefix('xmcontact_request') . ' WHERE request_status=0';
+    $result = $xoopsDB->query($sql);
+    if ($result) {
+        $block['adminlink'] = XOOPS_URL . '/modules/xmcontact/admin/request.php?request_cid=0&request_status=0';
+        [$block['pendingnum']] = $xoopsDB->fetchRow($result);
+        $block['lang_linkname'] = _PI_WAITING_CONTACT;
+    }
+
+    $ret[] = $block;
+
+    return $ret;
+}
